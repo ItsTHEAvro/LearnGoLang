89db33bcb2e1c5567cb9aba29abb7a25302c2c3a
Avro <42869374+ItsTHEAvro@users.noreply.github.com>
2025-08-09 20:59:29 +0600

    A simple go web server

---
+++ 01. Simple Web Server/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 Not Found", http.StatusNotFound)
+		return
+	}
+
+	if r.Method != "GET" {
+		http.Error(w, "Method is not supported", http.StatusNotFound)
+		return
+	}
+
+	fmt.Fprint(w, "Hello World!")
+}
+
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm error: %v", err)
+		return
+	}
+
+	fmt.Fprintf(w, "POST request successful!\n")
+
+	name := r.FormValue("name")
+	email := r.FormValue("email")
+
+	fmt.Fprintf(w, "Name = %s\n", name)
+	fmt.Fprintf(w, "Email = %s\n", email)
+}
+
+func main() {
+	fileServer := http.FileServer(http.Dir("./static/"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/submit", formHandler)
+	http.HandleFunc("/hello", helloHandler)
+
+	fmt.Println("Starting server at port 8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
